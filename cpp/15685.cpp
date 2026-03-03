8b53fab65d17902a77a5cfbb103025f11c1bf0dc
lamiro3 <lamiro3@naver.com>
2026-03-03 16:19:40 +0900

    [15685] 드래곤 커브 ~ implementation, simulation

--- cpp/15685.cpp
+++ cpp/15685.cpp
@@ -1,136 +1,57 @@
 #include <iostream>
-#include <map>
 #include <vector>
-#define point pair<int, int>
 
 using namespace std;
-typedef map<point, pair<point, bool>> Map;
 
-/*
-1. 오른쪽 위
-=> 0, 1
-2. (왼쪽 위)
-=> [0, 1], (2, 1)
-3. 왼쪽, 아래, (왼쪽, 위)
-=> [0, 1, 2, 1], 2, 3, (2, 1)
-4. 왼쪽, 아래, 오른쪽, 아래, (왼쪽, 아래, 왼쪽, 위)
-=> [0, 1, 2, 1, 2, 3, 2, 1], 2, 3, 0, 3, (2, 3, 2, 1)
-5. 왼쪽, 아래, 오른쪽, 아래, 오른쪽, 위, 오른쪽, 아래, (왼쪽, 아래, 오른쪽, 아래, 왼쪽, 아래, 왼쪽, 위)
-*/
-
-int N;
-Map grid;
-Map sq;
-
-// 오른쪽부터 시작해 반시계방향
+int board[101][101];
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, -1, 0, 1 };
 
-void SqCheck(point start) {
-	int x = start.first;
-	int y = start.second;
-	
-	int d2x[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
-	int d2y[8] = { 1, 1, 0, -1, -1, -1, 0, 1 };
-	bool isThere[8] = { false, };
-
-	for (int i = 0; i < 8; i++) {
-		int nx = x + d2x[i];
-		int ny = y + d2y[i];
-
-		if (grid[{nx, ny}].second)
-			isThere[i] = true;
-		else
-			isThere[i] = false;
-	}
-
-	// 현재 위치 기준 우 상단 
-	if (isThere[0] && isThere[1] && isThere[2]) {
-		sq[{x, y}].first = { x, y };
-		sq[{x, y}].second = true;
-	}
-
-	// 우 하단
-	else if (isThere[2] && isThere[3] && isThere[4]) {
-		sq[{x, y+1}].first = { x, y+1 };
-		sq[{x, y + 1}].second = true;
-	}
-
-	// 좌 하단
-	else if (isThere[4] && isThere[5] && isThere[6]) {
-		sq[{x-1, y+1}].first = { x-1, y+1 };
-		sq[{x - 1, y + 1}].second = true;
-	}
-
-	// 좌 상단
-	else if (isThere[6] && isThere[7] && isThere[0]) {
-		sq[{x-1, y}].first = { x-1, y };
-		sq[{x - 1, y}].second = true;
-	}
-}
-
-void GenDragon(point start, int init_dir, int gen) {
-	vector<int> history;
-	int size = 0;
-
-	for (int i = 0; i <= gen; i++) {
-		if (i == 0) {
-			history.push_back(init_dir);
-			size++;
-		}
-
-		else if (i == 1) {
-			history.push_back((init_dir + 1) % 4);
-			size *= 2;
-		}
-
-		else {
-			for (int j = 0; j < size; j++) {
-				history.push_back((history[j] + 2) % 4);
-			}
-
-			for (int j = 0; j < size; j++) {
-				history.push_back(history[j+size]);
-			}
-
-			size *= 2;
-		}
-	}
-
-	int nx = start.first;
-	int ny = start.second;
-	grid[start].first = start;
-	grid[start].second = true;
-
-	for (auto dir : history) {
-		nx += dx[dir];
-		ny += dy[dir];
-		grid[{nx, ny}].first = { nx, ny };
-		grid[{nx, ny}].second = true;
-	}
-}
-
 int main() {
-	cin >> N;
+    int N;
+    cin >> N;
+    while (N--) {
+        int x, y, d, g;
+        cin >> x >> y >> d >> g;
 
-	while (N--) {
-		// 시작점, 시작 방향, 세대
-		int x, y, d, g;
-		cin >> x >> y >> d >> g;
-		GenDragon({ x, y }, d, g);
-		cout << "gen is Done" << "\n";
-	}
+        vector<int> dirs;
+        dirs.push_back(d); // 0세대 방향
 
-	for (auto g: grid) {
-		auto [p, isThere] = g.second;
-		//cout << p.first << ", " << p.second << "\n";
-		SqCheck(p);
-	}
+        // g세대까지 방향 정보 생성
+        for (int i = 0; i < g; i++) {
+            for (int j = dirs.size() - 1; j >= 0; j--) {
+                dirs.push_back((dirs[j] + 1) % 4); // 역순으로 꺼내서 +1 (반시계 90도)
+            }
+        }
 
-	for (auto k : sq) {
-		auto [p, isThere] = k.second;
-		cout << p.first << p.second << "\n";
-	}
+        board[x][y] = 1;
 
-	return 0;
+        // 방문 처리
+        for (int dir : dirs) {
+            x += dx[dir];
+            y += dy[dir];
+            // 문제 똑바로 봐라 ㅋㅋㅋㅋ
+            if (x >= 0 && x <= 100 && y >= 0 && y <= 100) {
+                board[x][y] = 1;
+            }
+        }
+    }
+
+    // 사각형 개수
+    int cnt = 0;
+
+    /* 
+        범위에 음수도 포함되는 줄 알고 map으로 접근하는 바보 행동을 했었음
+        100행 100열이라 충분히 O(N^2)로 풀 수 있는 문제
+    */
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            if (board[i][j] && board[i + 1][j] && board[i][j + 1] && board[i + 1][j + 1]) {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << endl;
+
+    return 0;
 }
