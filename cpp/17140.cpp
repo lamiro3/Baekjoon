715bcb5fc3029255e4f968d5b305a84a418a86bd
lamiro3 <lamiro3@naver.com>
2026-03-05 22:11:27 +0900

    [17140] 이차원 배열과 연산 ~ implementation, sort

--- cpp/17140.cpp
+++ cpp/17140.cpp
@@ -1,86 +1,142 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <functional>
 
 using namespace std;
 
 int r, c, k;
-vector<pair<int, int>> info(101); // 숫자, 개수
-vector<vector<int>> A;
-
-void initinfo() {
-	for (int i = 1; i <= 100; i++) {
-		info[i].first = i;
-		info[i].second = 0;
-	}
-}
-
-
+int R = 3, C = 3;
+int A[101][101];
 /*
 	1. 숫자 개수 기준 오름차수 정렬
 	2. (1.에서 숫자 등장횟수가 같다면) 숫자 기준 오름차순 정렬
 */
-bool compare(int a, int b) {
-	if (info[a].second > info[b].second) {
-		return false;
+bool compare(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second) {
+		return a.first < b.first;
 	}
-
-	else if (info[a].second == info[b].second) {
-		if (a > b)
-			return false;
-		else
-			return true;
-	}
-
-	else
-		return true;
+	return a.second < b.second;
 }
 
-void row_sort(int row) {
-	initinfo();
+void row_sort() {
+	int nextC = 0;
 
-	for (int j = 0; j < A[row].size(); j++) {
-		info[A[row][j]].second++;
+	for (int i = 1; i <= R; i++) {
+		int cnt[101] = { 0, };
 
+		for (int j = 1; j <= C; j++) {
+			if (A[i][j] != 0) {
+				cnt[A[i][j]]++;
+			}
+		}
+
+		vector <pair<int, int>> info;
+
+		for (int num = 1; num <= 100; num++) {
+			if (cnt[num] > 0) {
+				info.push_back({ num, cnt[num] });
+			}
+		}
+
+		sort(info.begin(), info.end(), compare);
+		int index = 1;
+
+		for (auto [num, cnt] : info) {
+			if (index > 100)
+				break;
+
+			A[i][index++] = num;
+
+			if (index > 100)
+				break;
+
+			A[i][index++] = cnt;
+		}
+
+		nextC = max(nextC, index - 1);
+
+		// 나머지 빈공간 0으로 채우기
+		while (index <= 100) {
+			A[i][index++] = 0;
+		}
 	}
-	sort(info.begin(), info.end(), compare);
 
-	vector<int> temp;
-
-	for (auto [num, cnt] : info) {
-		if (cnt == 0)
-			continue;
-		temp.push_back(num);
-		temp.push_back(cnt);
-	}
+	C = min(100, nextC);
 }
 
-void col_sort(int col) {
-	initinfo();
-	for (int j = 0; i < j < A.size(); j++) {
-		info[A[j][col]].second++;
-	}
-	sort(info.begin(), info.end(), compare);
+void col_sort() {
+	int nextR = 0;
 
-	// 열 정렬 방식은 함 생각해봐야할 듯.. 시간 복잡도는 문제 없을 것 같음
+	for (int j = 1; j <= C; j++){
+		int cnt[101] = { 0, };
+		for (int i = 1; i <= R; i++) {
+			if (A[i][j] != 0) {
+				cnt[A[i][j]]++;
+			}
+		}
+
+		vector <pair<int, int>> info;
+
+		for (int num = 1; num <= 100; num++) {
+			if (cnt[num] > 0) {
+				info.push_back({ num, cnt[num] });
+			}
+		}
+
+		sort(info.begin(), info.end(), compare);
+
+		int index = 1;
+
+		for (auto [num, cnt] : info) {
+			if (index > 100)
+				break;
+
+			A[index++][j] = num;
+
+			if (index > 100)
+				break;
+
+			A[index++][j] = cnt;
+		}
+
+		nextR = max(nextR, index - 1);
+
+		while(index <= 100) {
+			A[index++][j] = 0;
+		}
+	}
+
+	R = min(100, nextR);
 }
 
 
 int main() {
+	int time = 0;
 	cin >> r >> c >> k;
 
-	for (int i = 0; i < 3; i++){
+	for (int i = 1; i <= 3; i++){
 		vector<int> temp;
-		for (int j = 0; j < 3; j++) {
-			int a;
-			cin >> a;
-			temp.push_back(a);
+		for (int j = 1; j <= 3; j++) {
+			cin >> A[i][j];
 		}
-		A.push_back(temp);
 	}
 
-	if (row >= col) {
+	while (time <= 100) {
+		if (A[r][c] == k && r<=R && c<=C) {
+			cout << time;
+			return 0;
+		}
 
+		if (R >= C) {
+			row_sort();
+		}
+		else {
+			col_sort();
+		}
+
+		time++;
 	}
+	
+	cout << -1;
+	return 0;
 }
